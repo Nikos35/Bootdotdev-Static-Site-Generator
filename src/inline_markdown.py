4b111941badc17496b055aedab184ff827d2bc63
Nikos-mar <niko.zojka@gmail.com>
2025-12-30 15:38:28 +0100

    tweaked split nodes image and link

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -31,27 +31,29 @@ def split_nodes_image(old_nodes):
             continue
         if old_node.text == "":
             continue
-        matches = extract_markdown_images(old_node.text)
-        if len(matches) == 0:
+        
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
             new_nodes.append(old_node)
+            continue
 
-        string_index = 0
-        for match in matches:
-            sections = old_node.text[string_index:].split(f"![{match[0]}]({match[1]})", 1)
-            # print(f"sections: {sections}, '[{match[0]}]({match[1]})'")
-            if len(sections[0]) > 0:
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            
+            if sections[0] != "":
                 new_nodes.append(TextNode(sections[0], TextType.TEXT))
             
-            new_nodes.append(TextNode(match[0], TextType.IMAGE, match[1]))
-            
-            string_index += len(sections[0]) + len(f"![{match[0]}]({match[1]})")
-        # print(string_index < len(old_node.text))
-    
-        if string_index < len(old_node.text):
-            new_nodes.append(TextNode(old_node.text[string_index:], TextType.TEXT)) 
-                   
-    return new_nodes
+            new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+            original_text = sections[1]
+        
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
 
+    return new_nodes
 
 def split_nodes_link(old_nodes):
     new_nodes = []
@@ -61,31 +63,30 @@ def split_nodes_link(old_nodes):
             continue
         if old_node.text == "":
             continue
-        matches = extract_markdown_links(old_node.text)
-        if len(matches) == 0:
+        
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
             new_nodes.append(old_node)
+            continue
 
-        string_index = 0
-        for match in matches:
-            sections = old_node.text[string_index:].split(f"[{match[0]}]({match[1]})", 1)
-            print(f"sections: {sections}, '[{match[0]}]({match[1]})'")
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
             
-            if len(sections[0]) > 0:
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            
+            if sections[0] != "":
                 new_nodes.append(TextNode(sections[0], TextType.TEXT))
-                
-            new_nodes.append(TextNode(match[0], TextType.LINK, match[1]))
-            string_index += len(sections[0]) + len(f"[{match[0]}]({match[1]})")
+            
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
         
-        print(string_index < len(old_node.text))
-        if string_index < len(old_node.text):
-            new_nodes.append(TextNode(old_node.text[string_index:], TextType.TEXT)) 
-                   
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+
     return new_nodes
-
-        
     
-
-
 def extract_markdown_images(text):
     pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern, text)
