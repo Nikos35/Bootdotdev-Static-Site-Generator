28dc99dbb6671a3ce425b3f1f0e24b8488e9968a
Nikos-mar <niko.zojka@gmail.com>
2026-01-07 21:57:01 +0100

    Added recursive file gen.

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,6 +1,40 @@
 import os
 
+from pathlib import Path
+
 from markdown_blocks import markdown_to_html_node
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
+
+
+
+# def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+
+#     os.makedirs(dest_dir_path, exist_ok=True)
+
+#     for file in os.listdir(dir_path_content):
+#         file_path = os.path.join(dir_path_content, file)
+
+#         print(f"analizing {file_path}")
+#         if os.path.isfile(file_path):
+#             print("itsafile")
+#             if not file.endswith(".md"):
+#                 continue
+#             html_file_name = file[:-2] + "html"
+#             generate_page(file_path, template_path, os.path.join(dest_dir_path, html_file_name))
+#         else:
+#             print("notafile")
+#             print(f"joint: {os.path.join(dest_dir_path, file)}")
+#             generate_pages_recursive(file_path, template_path, os.path.join(dest_dir_path, file))
+
 
 
 def generate_page(from_path, template_path, dest_path):
@@ -20,11 +54,10 @@ def generate_page(from_path, template_path, dest_path):
 
     html = template.replace("{{ Title }}", title)
     html = html.replace("{{ Content }}", content)
-
+    
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
-
     with open(dest_path, mode="w") as f:
         f.write(html)
     
