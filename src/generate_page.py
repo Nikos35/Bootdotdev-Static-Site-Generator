10f2aa94b35a5b373e1732d17e55034abcf80304
Nikos-mar <niko.zojka@gmail.com>
2026-01-07 22:10:26 +0100

    implemented gh pages suport

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,15 +4,15 @@ from pathlib import Path
 
 from markdown_blocks import markdown_to_html_node
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
 
@@ -37,7 +37,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
 
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
 
@@ -54,6 +54,8 @@ def generate_page(from_path, template_path, dest_path):
 
     html = template.replace("{{ Title }}", title)
     html = html.replace("{{ Content }}", content)
+    html = html.replace('href="/',f'href="{basepath}')
+    html = html.replace('src="/',f'src="{basepath}') #"?
     
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 from copystatic import copy_files_recursive, clear_directory
 
 from generate_page import generate_pages_recursive
+
+import sys
+
 def main():
+    basepath = sys.argv[0]
+    
+
+
+
     clear_directory("public")
     copy_files_recursive("static", "public")
 
-    generate_pages_recursive("content/", "template.html", "public/")
+    generate_pages_recursive("content/", "template.html", "docs/", basepath)
 
 
 
